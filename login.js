3bc155ddd36698ee5410a4e7af2083725e30fb85
immagical <104602961+immagical@users.noreply.github.com>
2022-12-16 14:34:22 -0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,7 @@
+function addUser() {
+    player1 = document.getElementById("player1input").value; 
+    player2 = document.getElementById("player2input").value; 
+    localStorage.setItem(player1, "player1")
+    localStorage.setItem(player2, "player2")
+    window.location = "game.html"; 
+}
